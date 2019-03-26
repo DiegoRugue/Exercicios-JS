80a2bf04fa44592b44da6f8b7b65102aba26c79c
Diego Whitringtom Fabbri Ruguê <38573217+DiegoRugue@users.noreply.github.com>
2019-03-26 14:14:24 -0300

    Add files via upload
    26/03/2019

--- Medio_3.js
+++ Medio_3.js
@@ -3,6 +3,28 @@
  * e calcule as N linhas do triângulo de pascal.
 */ 
 
+function fatorial(n){
+    if (n <= 1){
+        return 1;
+    } else {
+        return n * fatorial(n - 1);
+    }
+}
+
+function triangulo(n){
+    let matriz = [];
+    for (let i = 0; i < n; i++) {
+        matriz[i] = [];
+        matriz[i][0] = 1;
+        matriz[i][i] = 1;
+        for (let j = 1; j < i; j++) {
+            matriz[i][j] = fatorial(i) / (fatorial(i - j) * fatorial(j));
+        }
+        console.log(matriz[i] + '\n');
+    }
+}
+
+triangulo(7);
 
 
 
