62e5271f23bd7078a63e3b00f5a2ce9cd2a7874c
Diego Whitringtom Fabbri Ruguê <38573217+DiegoRugue@users.noreply.github.com>
2019-03-26 16:59:27 -0300

    Add files via upload

---
+++ Medio_7.js
@@ -0,0 +1,26 @@
+// 7) Faça um algoritmo para calcular a transposta de uma matriz.
+
+function transposta() {
+    let matriz = [];
+    let matriz2 = [];
+    let n = 1;
+    let n2 = 1;
+    for (let i = 0; i < 5; i++) {
+        matriz[i] = [];
+        for (let j = 0; j < 5; j++) {
+            matriz[i][j] = n ++;
+        }
+    }
+
+    for (let i = 0; i < 5; i++) {
+        matriz2[i] = [];
+        for (let j = 0; j < 5; j++) {
+            matriz2[i][j] = matriz[j][i];
+        }
+        
+    }
+    console.log(matriz)
+    console.log(matriz2)
+}
+
+transposta();
